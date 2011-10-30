ed02b42831dee0ea2df9e6e0247e76618eb8d4de
Carlo Emil Angiuli <carlo@carloangiuli.com>
2011-10-30 14:56:28 -0400

    Some cleanup.

--- src/CLib.hs
+++ src/CLib.hs
@@ -1,4 +1,5 @@
 -- Wrappers for Language.C
+-- Only functions starting with 'c' should be used externally.
 
 module CLib where
 
@@ -7,7 +8,7 @@ import qualified Language.C
 
 -- Miscellaneous {{{
 
-cTopLevel ns = CTranslUnit ns undefNode
+cFile ns = CTranslUnit ns undefNode
 ident = internalIdent
 
 -- }}}
@@ -23,46 +24,43 @@ cUnsig = CTypeSpec $ CUnsigType undefNode
 cBool = CTypeSpec $ CBoolType undefNode
 
 -- Structs and unions
-cStruct id = cStructUnion CStructTag id Nothing
-cUnion id = cStructUnion CUnionTag id Nothing
+cStruct id = structUnion CStructTag id Nothing
+cUnion id = structUnion CUnionTag id Nothing
 
-cStructUnion tag id decls = CTypeSpec $ CSUType
+structUnion tag id decls = CTypeSpec $ CSUType
   (CStruct tag (Just $ ident id) decls [] undefNode) undefNode
 
 -- Enums
-cEnum id = cEnum' id Nothing
+cEnum id = enum id Nothing
 
-cEnum' id enumls = CTypeSpec $ CEnumType
+enum id enumls = CTypeSpec $ CEnumType
   (CEnum (Just $ ident id) enumls [] undefNode) undefNode
 
 -- }}}
 -- Declarations {{{
 
-cDecl typ = cDecl' [typ]
-cDeclStatic typ = cDecl' [cStatic,typ]
-cDeclExtern typ = cDecl' [cExtern,typ]
+cDecl typ = decl [typ]
+cDeclStatic typ = decl [cStatic,typ]
+cDeclExtern typ = decl [cExtern,typ]
 
-cDecl' typSpecs indirs id init = CDecl typSpecs
+cDeclExt = CDeclExt
+
+decl typSpecs indirs id init = CDecl typSpecs
   [(Just $ cDeclr id indirs,initExpr,Nothing)] undefNode
   where initExpr = init >>= (Just . cInit)
 
--- Top-level declarations
-cTopDecl typ indirs id init = CDeclExt $ cDecl typ indirs id init
-cTopDeclStatic typ indirs id init = CDeclExt $ cDeclStatic typ indirs id init
-cTopDeclExtern typ indirs id init = CDeclExt $ cDeclExtern typ indirs id init
-
 -- Type name declarations
-cTypeDecl indirs declr = CDeclExt $ CDecl [declr]
+typeDecl indirs declr = CDeclExt $ CDecl [declr]
   [(Just $ cAbstractDeclr indirs,Nothing,Nothing)] undefNode
 
 -- Typedefs
-cTypedef indirs typ id = CDeclExt $ cDecl'
+cTypedef indirs typ id = CDeclExt $ decl
   [CStorageSpec $ CTypedef undefNode,typ] indirs id Nothing
 
 -- Structure declarations
-cStructDecl id decls = cTypeDecl [] $ cStructUnion CStructTag id (Just decls)
-cUnionDecl id decls = cTypeDecl [] $ cStructUnion CUnionTag id (Just decls)
-cEnumDecl id idexps = cTypeDecl [] $ cEnum' id (Just ls)
+cStructDecl id decls = typeDecl [] $ structUnion CStructTag id (Just decls)
+cUnionDecl id decls = typeDecl [] $ structUnion CUnionTag id (Just decls)
+cEnumDecl id idexps = typeDecl [] $ enum id (Just ls)
   where ls = map (\(id,expr) -> (ident id,expr)) idexps
 
 -- Declarators
@@ -82,7 +80,7 @@ cExtern = CStorageSpec $ CExtern undefNode
 cFunction id params retTyp body = CFDefExt $ CFunDef
   [retTyp] (cDeclr id [cFun $ map f params]) [] body undefNode where
   cFun params = CFunDeclr (Right (params,False)) [] undefNode
-  f (typ,id) = cDecl' [typ] [] id Nothing
+  f (typ,id) = decl [typ] [] id Nothing
 
 -- Initializers
 cInit expr = CInitExpr expr undefNode
@@ -90,61 +88,58 @@ cInit expr = CInitExpr expr undefNode
 -- }}}
 -- Expressions {{{
 
-cComma exprs = CComma exprs undefNode
-
 -- Assignment operators
-cAssignOp assop lhs rhs = CAssign assop lhs rhs undefNode 
-cAssign = cAssignOp CAssignOp
-cMulEq = cAssignOp CMulAssOp
-cDivEq = cAssignOp CDivAssOp
-cModEq = cAssignOp CRmdAssOp
-cAddEq = cAssignOp CAddAssOp
-cSubEq = cAssignOp CSubAssOp
-cShlEq = cAssignOp CShlAssOp
-cShrEq = cAssignOp CShrAssOp
-cAndEq = cAssignOp CAndAssOp
-cXorEq = cAssignOp CXorAssOp
-cOrEq = cAssignOp COrAssOp
-
-cTernary test conseq alt = CCond test (Just conseq) alt undefNode
-
--- Binary operators
-cBinaryOp binop expr1 expr2 = CBinary binop expr1 expr2 undefNode
-cMul = cBinaryOp CMulOp
-cDiv = cBinaryOp CDivOp
-cMod = cBinaryOp CRmdOp
-cAdd = cBinaryOp CAddOp
-cSub = cBinaryOp CSubOp
-cShl = cBinaryOp CShlOp
-cShr = cBinaryOp CShrOp
-cLt = cBinaryOp CLeOp
-cGt = cBinaryOp CGrOp
-cLeq = cBinaryOp CLeqOp
-cGeq = cBinaryOp CGeqOp
-cEq = cBinaryOp CEqOp
-cNeq = cBinaryOp CNeqOp
-cAnd = cBinaryOp CAndOp
-cXor = cBinaryOp CXorOp
-cOr = cBinaryOp COrOp
-cLand = cBinaryOp CLndOp
-cLor = cBinaryOp CLorOp
-
-cCast typ expr = CCast (CDecl [typ] [] undefNode) expr undefNode
+assignOp assop lhs rhs = CAssign assop lhs rhs undefNode 
+cAssign = assignOp CAssignOp
+cMulEq = assignOp CMulAssOp
+cDivEq = assignOp CDivAssOp
+cModEq = assignOp CRmdAssOp
+cAddEq = assignOp CAddAssOp
+cSubEq = assignOp CSubAssOp
+cShlEq = assignOp CShlAssOp
+cShrEq = assignOp CShrAssOp
+cAndEq = assignOp CAndAssOp
+cXorEq = assignOp CXorAssOp
+cOrEq = assignOp COrAssOp
 
 -- Unary operators
-cUnaryOp unop expr = CUnary unop expr undefNode
-cPreInc = cUnaryOp CPreIncOp
-cPreDec = cUnaryOp CPreDecOp
-cPostInc = cUnaryOp CPostIncOp
-cPostDec = cUnaryOp CPostDecOp
-cAddr = cUnaryOp CAdrOp
-cDeref = cUnaryOp CIndOp
-cNot = cUnaryOp CCompOp
-cLnot = cUnaryOp CNegOp
+unaryOp op expr = CUnary op expr undefNode
+cPreInc = unaryOp CPreIncOp
+cPreDec = unaryOp CPreDecOp
+cPostInc = unaryOp CPostIncOp
+cPostDec = unaryOp CPostDecOp
+cAddr = unaryOp CAdrOp
+cDeref = unaryOp CIndOp
+cNot = unaryOp CCompOp
+cLnot = unaryOp CNegOp
 
+-- Binary operators
+binaryOp op expr1 expr2 = CBinary op expr1 expr2 undefNode
+cMul = binaryOp CMulOp
+cDiv = binaryOp CDivOp
+cMod = binaryOp CRmdOp
+cAdd = binaryOp CAddOp
+cSub = binaryOp CSubOp
+cShl = binaryOp CShlOp
+cShr = binaryOp CShrOp
+cLt = binaryOp CLeOp
+cGt = binaryOp CGrOp
+cLeq = binaryOp CLeqOp
+cGeq = binaryOp CGeqOp
+cEq = binaryOp CEqOp
+cNeq = binaryOp CNeqOp
+cAnd = binaryOp CAndOp
+cXor = binaryOp CXorOp
+cOr = binaryOp COrOp
+cLand = binaryOp CLndOp
+cLor = binaryOp CLorOp
+
+cCast typ expr = CCast (CDecl [typ] [] undefNode) expr undefNode
 cSizeofExpr expr = CSizeofExpr expr undefNode
 cSizeofType typ = CSizeofType (CDecl [typ] [] undefNode) undefNode
 
+cTernary test conseq alt = CCond test (Just conseq) alt undefNode
+cComma exprs = CComma exprs undefNode
 cIndex arr ind = CIndex arr ind undefNode
 cCall fun args = CCall fun args undefNode
 cDot expr id = CMember expr (ident id) False undefNode
@@ -162,10 +157,11 @@ cStrConst x = CConst $ CStrConst (cString x) undefNode
 -- Switch
 cCase expr stmt = CCase expr stmt undefNode
 cDefault stmt = CDefault stmt undefNode
-cSwitch' expr stmt = CSwitch expr stmt undefNode
+cSwitch expr stmt = CSwitch expr stmt undefNode
 cBreak = CBreak undefNode
--- CExpr -> [(Maybe CExpression, [CStatement])] -> CStatement
-cSwitch expr cases = cSwitch' expr (cCompound (map (Left . f) cases)) where
+-- :: CExpr -> [(Maybe CExpression, [CStatement])] -> CStatement
+cSwitchBlock expr cases = cSwitch expr (cCompound (map (Left . f) cases))
+  where
   f (Nothing,stmts) = cDefault $ cCompound $ map Left (stmts ++ [cBreak])
   f (Just expr,stmts) = cCase expr $ cCompound $ map Left (stmts ++ [cBreak])
 
@@ -175,14 +171,12 @@ cExpr expr = CExpr (Just expr) undefNode
 cCompound stmts = CCompound [] stmts' undefNode where
   stmts' = map (either CBlockStmt CBlockDecl) stmts
 
-cIfThen expr conseq = CIf expr conseq Nothing undefNode
-cIfThenElse expr conseq alt = CIf expr conseq (Just alt) undefNode
+cIfThen expr conseq altM = CIf expr conseq altM undefNode
 
 -- Looping constructs
 cWhile expr stmt = CWhile expr stmt False undefNode
 cDoWhile expr stmt = CWhile expr stmt True undefNode
-cFor expr1 expr2 expr3 stmt = CFor (Left expr1) expr2 expr3 stmt undefNode
-cForDecl decl expr2 expr3 stmt = CFor (Right decl) expr2 expr3 stmt undefNode
+cFor exprDeclE expr2 expr3 stmt = CFor exprDeclE expr2 expr3 stmt undefNode
 cContinue = CCont undefNode
 
 cReturn expr = CReturn (Just expr) undefNode
@@ -191,53 +185,51 @@ cReturnVoid = CReturn Nothing undefNode
 -- }}}
 -- Tests {{{
 
-test1 = print $ pretty $ cTopLevel
-  [cTopDecl cInt [] "x" Nothing]
+test1 = print $ pretty $ cFile
+  [cDeclExt $ cDecl cInt [] "x" Nothing]
 
-test2 = print $ pretty $ cTopLevel
+test2 = print $ pretty $ cFile
   [cFunction "main" [] cInt
    (cCompound [Left (cReturn (cIntConst 0))])]
 
-test3 = print $ pretty $ cTopLevel
+test3 = print $ pretty $ cFile
   [cFunction "main" [] cInt
    (cCompound [Left (cExpr (cAssign (cVar "x") (cIntConst 2)))])]
 
-test4 = print $ pretty $ cTopLevel
+test4 = print $ pretty $ cFile
   [cFunction "main" [] cVoid
    (cCompound [Left (cExpr (cCall (cVar "f") [cIntConst 0])),
                Left (cExpr (cIndex (cVar "a") (cIntConst 1))),
                Left (cExpr (cDot (cVar "x") "foo")),
                Left (cExpr (cTernary (cVar "x") (cVar "x") (cVar "x"))),
                Left (cExpr (cComma [cVar "x",cVar "y"])),
-               Left cReturnVoid
-               ])]
+               Left cReturnVoid])]
 
-test5 = print $ pretty $ cTopLevel
+test5 = print $ pretty $ cFile
   [cFunction "main" [] cInt
-   (cCompound [Left (cSwitch (cVar "f")
+   (cCompound [Left (cSwitchBlock (cVar "f")
                      [(Just (cIntConst 1),[cExpr $ cVar "x"]),
                       (Nothing,[cExpr $ cVar "y"])]),
                Left (cWhile (cVar "x") (cExpr $ cVar "y")),
-               Left (cFor (Just (cVar "x")) (Just (cVar "y")) (Just (cVar "z"))
+               Left (cFor (Left $ Just (cVar "x"))
+                          (Just (cVar "y")) (Just (cVar "z"))
                            (cExpr $ cIntConst 0)),
-               Left (cReturn (cIntConst 0))
-               ])]
+               Left (cReturn (cIntConst 0))])]
 
-test6 = print $ pretty $ cTopLevel
+test6 = print $ pretty $ cFile
   [cStructDecl "Node"
      [cDecl cInt [] "data" Nothing,
      cDecl (cStruct "Node") [cPtr] "next" Nothing],
    cEnumDecl "Color" [("red",Nothing),("green",Just $ cIntConst 5)],
    cTypedef [cPtr] (cStruct "Node") "nodeptr",
    cTypedef [] (cEnum "Color") "enumcolor",
-   cTopDecl cChar [] "a" Nothing,
+   cDeclExt $ cDecl cChar [] "a" Nothing,
    cFunction "main" [(cInt,"x")] cInt
    (cCompound [Right $ cDecl cChar [] "a" Nothing,
                Right $ cDecl cInt [cArray $ Just $ cIntConst 5] "b" Nothing,
                Right $ cDecl cInt [cPtr] "c" (Just $ cIntConst 0),
                Right $ cDecl cInt [cPtr] "c" (Just $ cVar "null"),
                Left $ cExpr $ cCast cInt (cIntConst 0),
-               Left $ cExpr $ cSizeofType cInt
-               ])]
+               Left $ cExpr $ cSizeofType cInt])]
 
 -- }}}
