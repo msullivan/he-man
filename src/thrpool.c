c24d41b95ebf25cb402a4c532fc74a0601023f2d
Michael Sullivan <sully@msully.net>
2011-11-05 17:59:37 -0400

    Add some old thrpool code.

---
+++ src/thrpool.c
@@ -0,0 +1,130 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include "variable_queue.h"
+#include "thrpool.h"
+
+static void thread_pool_spawn_new(thread_pool_t *pool, int id);
+static void *thread_pool_bottom(void *data);
+
+int thread_pool_init(thread_pool_t **pool, work_func *func, int max_threads)
+{
+	int i, num;
+	
+	/* XXX: things could fail... */
+	thread_pool_t *p = malloc(sizeof(thread_pool_t));
+	
+	p->func = func;
+	p->num_threads = 0;
+	p->max_threads = max_threads;
+	Q_INIT_HEAD(&p->idle_threads);
+	Q_INIT_HEAD(&p->working_threads);
+	Q_INIT_HEAD(&p->work_queue);
+	pthread_mutex_init(&p->pool_lock, NULL);
+	pthread_cond_init(&p->work_ready, NULL); /*XXX*/
+
+	/* spawn up our threads - XXX: this is special. */
+	num = p->max_threads;
+	for (i = 0; i < num; i++)
+		thread_pool_spawn_new(p, i+1);
+
+	*pool = p;	
+	return 0;
+}
+
+static void thread_pool_spawn_new(thread_pool_t *pool, int id)
+{
+	thread_t *thr = malloc(sizeof(thread_t));
+	Q_INIT_ELEM(thr, q_link);
+	thr->id = id;
+	thr->pool = pool;
+	pthread_create(&thr->thread, NULL, thread_pool_bottom, thr);
+}
+
+static void *thread_pool_bottom(void *data)
+{
+	thread_t *me = data;
+	thread_pool_t *pool = me->pool;
+	work_item_t *work;
+
+	pthread_mutex_lock(&pool->pool_lock);
+	pool->num_threads++;
+	Q_INSERT_FRONT(&pool->idle_threads, me, q_link);
+
+	/* and, loop for jobs */
+	for (;;) {
+		while ((work = Q_GET_FRONT(&pool->work_queue)) == NULL)
+			pthread_cond_wait(&pool->work_ready, &pool->pool_lock);
+
+		/* grab work */
+		Q_REMOVE(&pool->work_queue, work, q_link);
+		/* update the thread lists */
+		Q_REMOVE(&pool->idle_threads, me, q_link);
+		Q_INSERT_FRONT(&pool->working_threads, me, q_link);
+		me->current_work = work;
+		
+		/* unlock the pool, then work */
+		pthread_mutex_unlock(&pool->pool_lock);
+
+		pool->func(me->current_work);
+
+		pthread_mutex_lock(&pool->pool_lock);
+
+		/* update the thread lists */
+		Q_REMOVE(&pool->working_threads, me, q_link);
+		Q_INSERT_FRONT(&pool->idle_threads, me, q_link);
+		me->current_work = NULL;
+	}
+
+	abort();
+	return NULL;
+}
+
+int thread_pool_push(thread_pool_t *pool, void *data)
+{
+	work_item_t *item = data;
+	Q_INIT_ELEM(item, q_link);
+
+	pthread_mutex_lock(&pool->pool_lock);
+	
+	Q_INSERT_TAIL(&pool->work_queue, item, q_link);
+	pthread_cond_signal(&pool->work_ready);
+
+	pthread_mutex_unlock(&pool->pool_lock);
+	
+	return 0;
+}
+
+void thread_pool_debug(thread_pool_t *pool, print_func *print, FILE *out)
+{
+	thread_t *thr;
+	work_item_t *work;
+	
+	pthread_mutex_lock(&pool->pool_lock);
+
+	fprintf(out, "==========================\n");
+
+	fprintf(out, "idle threads: ");
+	Q_FOREACH(thr, &pool->idle_threads, q_link) {
+		fprintf(out, "%d", thr->id);
+		if (Q_GET_NEXT(thr, q_link)) fprintf(out, ", ");
+	}
+
+	fprintf(out, "\nworking threads: ");
+	Q_FOREACH(thr, &pool->working_threads, q_link) {
+		fprintf(out, "%d: ", thr->id);
+		print(thr->current_work, out);
+		if (Q_GET_NEXT(thr, q_link)) fprintf(out, ", ");
+	}
+
+	fprintf(out, "\ndata in queue: ");
+	Q_FOREACH(work, &pool->work_queue, q_link) {
+		print(work, out);
+		if (Q_GET_NEXT(work, q_link)) fprintf(out, ", ");
+	}
+
+
+	fprintf(out, "\n==========================\n");
+
+	pthread_mutex_unlock(&pool->pool_lock);
+}
---
+++ src/thrpool.h
@@ -0,0 +1,44 @@
+#ifndef THRPOOL_H
+#define THRPOOL_H
+
+#include <pthread.h>
+#include <stdio.h>
+#include "variable_queue.h"
+
+/* The data being handles *must* begin with a work_item! */
+
+typedef void (work_func)(void *);
+typedef void (print_func)(void *, FILE *);
+
+typedef struct work_item_t {
+	Q_NEW_LINK(work_item_t) q_link;
+} work_item_t;
+
+typedef struct thread_t {
+	pthread_t thread;
+	int id;
+	void *current_work;
+	struct thread_pool_t *pool;
+	Q_NEW_LINK(thread_t) q_link;
+} thread_t;
+
+Q_NEW_HEAD(work_queue_t, work_item_t);
+Q_NEW_HEAD(thread_list_t, thread_t);
+
+typedef struct thread_pool_t {
+	work_func *func;
+	int num_threads;
+	int max_threads;
+	thread_list_t working_threads;
+	thread_list_t idle_threads;
+	work_queue_t work_queue;
+	pthread_mutex_t pool_lock;
+	pthread_cond_t work_ready;
+} thread_pool_t;
+
+int thread_pool_init(thread_pool_t **pool, work_func *func, int max_threads);
+int thread_pool_push(thread_pool_t *pool, void *data);
+void thread_pool_debug(thread_pool_t *pool, print_func *print, FILE *out);
+
+#endif
+
