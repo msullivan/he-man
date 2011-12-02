b09137e5035ac5e4998f898efa9c74266bb12e01
Carlo Emil Angiuli <carlo@carloangiuli.com>
2011-12-02 16:48:44 -0500

    Work on codegen for Spawn

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -16,7 +16,7 @@ codegen (blocks, threads) =
 hasVar vars x = isJust $ find (\(y, _) -> x == y) vars
 
 blockName id = "block" ++ show id
-threadName id = "thread" ++ show id
+threadName id = "thread" ++ show id ++ "_t"
 
 --}}}
 --{{{ Grammar
@@ -24,24 +24,24 @@ threadName id = "thread" ++ show id
 translateThread (name, vars) =
   cTypedef (cStructType sname (map declVar vars)) [] tydefName
   where declVar (x, t) = cDecl [translateType t] [] x Nothing
-        sname = threadName name ++ "_t"
+        sname = threadName name
         -- This is a frumious hack to sneak in a toplevel invocation of
         -- DECLARE_THREAD
-        tydefName = sname ++ "; DECLARE_THREAD(" ++ sname ++ ")"
+        tydefName = sname ++ ";\nDECLARE_THREAD(" ++ sname ++ ")"
 
 translateBlock threads (id, thread, stmts, tail) =
   cFunction (blockName id) [([cType "thread_t"],[cPtr],"thread")] [cInt]
   (cCompound (
-      [Right $ cDecl [threadType] [] "thread_priv" 
+      [Right $ cDecl [threadType] [cPtr] "priv" 
        (Just (cCast threadType [cPtr] (cVar "thread")))] ++
-      map (translateStmt vars) stmts ++
+      concatMap (translateStmt vars) stmts ++
       translateTail vars tail))
     where Just vars = lookup thread threads
-          threadType = cType (threadName thread ++ "_t")
+          threadType = cType (threadName thread)
 
 translateExpr vars expr =
   case expr of
-    Var x -> if hasVar vars x then cArrow (cVar "thread_priv") x else cVar x
+    Var x -> if hasVar vars x then cArrow (cVar "priv") x else cVar x
     Call (CFn f) args -> cCall (cVar f) (map trans args)
     Arith op e e' -> (translateArith op) (trans e) (trans e')
     ArithUnop op e -> (translateArithUnop op) (trans e)
@@ -56,13 +56,23 @@ translateStmt vars stmt =
   case stmt of
     Decl (x, t) e ->
       if hasVar vars x then
-        Left $ cExpr (cAssign (cVar x) (transE e))
+        [Left $ cExpr (cAssign (cVar x) (transE e))]
       else
-        Right $ cDecl [translateType t] [] x (Just $ transE e)
-    Assign e1 e2 -> Left $ cExpr (cAssign (transE e1) (transE e2))
-    Exp e -> Left $ cExpr $ transE e
-    Spawn thd _ -> Left $ cExpr $ cCall (cVar $ "mk_" ++ threadName thd) []
-    -- TODO Spawn
+        [Right $ cDecl [translateType t] [] x (Just $ transE e)]
+    Assign e1 e2 -> [Left $ cExpr (cAssign (transE e1) (transE e2))]
+    Exp e -> [Left $ cExpr $ transE e]
+    Spawn thd args -> makeT:setupTVars ++ setupTRun where
+      thread = threadName thd
+      makeT = Right $ cDecl [cType thread] [cPtr] "new_thread" $ 
+        Just $ cCall (cVar $ "mk_" ++ thread) []
+      setupTVars = map (\((var,_),arg) ->
+        Left $ cExpr $ cAssign (cArrow (cVar "new_thread") var) (transE arg))
+        args
+      setupTRun = [Left $ cExpr $ cAssign
+          (cDot (cArrow (cVar "new_thread") "thread") "cont")
+          (cVar $ blockName thd),
+        Left $ cExpr $ cCall (cVar "make_runnable") $
+          [cAddr (cArrow (cVar "new_thread") "thread")]]
     where transE = translateExpr vars
 
 translateTail vars tail =
@@ -73,7 +83,6 @@ translateTail vars tail =
     If e t1 t2 -> [Left $
       cIfThen (translateExpr vars e) (cCompound (translateTail vars t1))
       (Just (cCompound (translateTail vars t2)))]
-
     where return n = Left $ cReturn (cIntConst n)
           jump target = Left $ cExpr (cAssign
                                       (cArrow (cVar "thread") "cont")
